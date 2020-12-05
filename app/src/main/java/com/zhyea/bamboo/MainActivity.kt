63583f9d6c89901a3633572bef4d5d42f739bacc
zhyea <rui.zhang@tendcloud.com>
2020-12-05 22:15:38 +0800

    learn
    Change-Id: I312f7a3ad8054ebf407180de1539d9da6c9788f8

--- app/src/main/java/com/zhyea/bamboo/MainActivity.kt
+++ app/src/main/java/com/zhyea/bamboo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.zhyea.bamboo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.hello)
+        text.text = "Hello Robin!"
     }
 }
