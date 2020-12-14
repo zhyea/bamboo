f1b16bacc5e35f35957ddef4854db326946acfc6
zhyea <zhangrui@zhyea.com>
2020-12-15 06:45:45 +0800

    learn 20201215
    Change-Id: Idb37e8f78599034df2402e52fe19fc981394551b

--- app/src/main/java/com/zhyea/bamboo/general/CustomDialog.kt
+++ app/src/main/java/com/zhyea/bamboo/general/CustomDialog.kt
@@ -1,10 +1,21 @@
 package com.zhyea.bamboo.general
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 
 /**
  * er
  */
-class CustomDialog(context: Context, themeResId: Int) : Dialog(context) {
+class CustomDialog(context: Context, themeResId: Int) : Dialog(context, themeResId) {
+
+
+    private fun onUserInteraction() {
+        if (context is Activity) {
+            val a = context as Activity
+            a.onUserInteraction()
+        }
+    }
+
+
 }
