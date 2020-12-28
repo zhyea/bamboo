5a29d0f8d7b612ca1fea6d9b53674ed3162ce85f
zhyea <zhangrui@zhyea.com>
2020-12-29 07:23:54 +0800

    learn 20201229
    Change-Id: Ic236d90241680c196052e73a205892a2b88ff033

--- app/src/main/java/com/zhyea/bamboo/reader/BambooActivity.kt
+++ app/src/main/java/com/zhyea/bamboo/reader/BambooActivity.kt
@@ -1,12 +1,15 @@
 package com.zhyea.bamboo.reader
 
 import android.app.Activity
+import android.os.Handler
+import android.os.Looper
 import android.os.PowerManager
 import android.os.PowerManager.WakeLock
 import android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
 import com.zhyea.bamboo.general.ActivatedHandler
 import com.zhyea.bamboo.general.IRequestHandler
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class BambooActivity : Activity() {
@@ -14,8 +17,47 @@ class BambooActivity : Activity() {
 
     private val listener: BambooActivityListener = BambooActivityListener(this)
 
+    /**
+     * n
+     */
     private var wakeLock: WakeLock? = null
 
+    /**
+     * o
+     */
+    private var handler: Handler? = null
+
+    /**
+     * d
+     */
+    private val windowRotationChangeListeners: LinkedList<OnWindowRotationChangeListener> = LinkedList()
+
+    /**
+     * p
+     */
+    private val delayMillis = 0;
+
+
+    private fun lockScreen() {
+        getScreenLock().acquire(TimeUnit.MINUTES.toMillis(1))
+    }
+
+    private fun unlockScreen() {
+        getScreenLock().release()
+    }
+
+    private fun resetScreenTimeout() {
+        if (null == this.handler) {
+            this.handler = Handler(Looper.getMainLooper(), new DkActivity .3(this))
+        }
+        this.handler!!.removeCallbacksAndMessages(null);
+        if (0 == delayMillis) {
+            unlockScreen();
+        }
+        if ((0 != this.delayMillis) && (this.delayMillis != Int.MAX_VALUE)) {
+            this.handler!!.sendEmptyMessageDelayed(0, this.delayMillis.toLong())
+        }
+    }
 
     private fun getScreenLock(): WakeLock {
         if (this.wakeLock == null) {
@@ -26,11 +68,20 @@ class BambooActivity : Activity() {
         return this.wakeLock!!
     }
 
-
-    private fun unlockScreen() {
-        getScreenLock().release()
+    private fun closeScreenTimeout() {
+        this.handler?.removeCallbacksAndMessages(null)
     }
 
+
+    private fun notifyWindowRotationChange(eventId: Int) {
+        for (listener in windowRotationChangeListeners) {
+            listener.onWindowRotationChange(eventId)
+        }
+    }
+
+    /**
+     * -------------------------------------------------------------------------
+     */
 
     class BambooActivityTask : TimerTask() {
         override fun run() {
@@ -58,6 +109,11 @@ class BambooActivity : Activity() {
             return this.activity.window.attributes.softInputMode
         }
 
+    }
+
+
+    interface OnWindowRotationChangeListener {
+        fun onWindowRotationChange(eventId: Int)
     }
 
 }
